8e7e2173169377fe0e225340ae2ab37e844d8b6a
Hakan <hknpeksen@gmail.com>
2022-05-25 23:10:34 +0300

    Initial Commit

---
+++ Week2_Homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week2_Homework
+//
+//  Created by Hakan Pekşen on 25.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
